4919a8b19807cba9d0dd5fc92281c2fff61e4d13
TanJohn <lehoangtan1604@gmail.com>
2022-10-16 16:22:34 +0700

    thêm code servo, điều khiển servo bằng kích cạnh lên

--- testserial_v1.2/testserial_v1.2.ino
+++ testserial_v1.2/testserial_v1.2.ino
@@ -1,3 +1,4 @@
+#include <Servo.h>
 #define numOfValsRec 3
 #define digitsPerValRec 3
 int valsRec[numOfValsRec];
@@ -9,24 +10,32 @@ String receivedString;
 String myS="123,456";
 int flag=1;
 int sta=0;
-int led5=5;
-int led6=6;
+
 int pushButton = 2;
 int pushButton2 = 3;
 int buttonState;
 int buttonState2;
+
+
+int curcheckservo1=0;
+int curcheckservo2=0;
+int precheckservo1=0;
+int precheckservo2=0;
+int statusservo1=0;
+int statusservo2=0;
+
+Servo myservo1;
+Servo myservo2;
 void setup() {
+valsRec[0]=1;
+valsRec[1]=0;
+valsRec[2]=1;
 Serial.begin(9600);
 pinMode(LED_BUILTIN, OUTPUT);
-pinMode(7, INPUT);
-valsRec[0]=1;
-valsRec[1]=1;
-valsRec[2]=1;
 pinMode(pushButton, INPUT);
 pinMode(pushButton2, INPUT);
-
-pinMode(led5, OUTPUT);
-pinMode(led6, OUTPUT);
+myservo1.attach(9); 
+myservo2.attach(10); 
 }
 
 void receiveData(){
@@ -87,13 +96,88 @@ buttonState = digitalRead(pushButton);
 buttonState2 = digitalRead(pushButton2);
 
 valsRec[0]= buttonState * 1 + buttonState2*10;
-valsRec[1]=111;
+
 valsRec[2]=222;
-// print out the state of the button:
+
+// servo1
+
+if(valsRec[1] & 1){
+  curcheckservo1=1;
+}
+else{
+  curcheckservo1=0;
+}
+
+if(valsRec[1] & 10){
+  curcheckservo2=1;
+}
+else{
+  curcheckservo2=0;
+}
+
+if(precheckservo1 == curcheckservo1){
+  statusservo1=0;
+}
+
+if(precheckservo1 == 0 && curcheckservo1 == 1){
+  statusservo1=1;
+}
+
+if(precheckservo2 == curcheckservo2){
+  statusservo2=0;
+}
+
+if(precheckservo2 == 0 && curcheckservo2 == 1){
+  statusservo2=1;
+}
 
 
-delay(5);        // delay in between reads for stability
 
+
+if( statusservo1 && statusservo2){
+  for(int i=0;i<=180;i++){
+    myservo1.write(i);
+    myservo2.write(i); 
+    delay(3);
+  }
+for (int i = 180; i >= 0; i -= 1) { 
+    myservo1.write(i);   
+    myservo2.write(i);           
+    delay(3);
+  }
+  
+}
+
+else if( statusservo1){
+  for(int i=0;i<=180;i++){
+    myservo1.write(i);
+
+    delay(3);
+  }
+for (int i = 180; i >= 0; i -= 1) { 
+    myservo1.write(i);   
+         
+    delay(3);
+  }
+  
+}
+
+else if( statusservo2 ){
+  for(int i=0;i<=180;i++){
+
+    myservo2.write(i); 
+    delay(3);
+  }
+for (int i = 180; i >= 0; i -= 1) { 
+ 
+    myservo2.write(i);           
+    delay(3);
+  }
+  
+}
+
+precheckservo1 = curcheckservo1;
+precheckservo2 = curcheckservo2;
   
 //-----------------------------end do something -------------------------------
 }
